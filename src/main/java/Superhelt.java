b1e8c71f7b834e28f11ba95c441e32c41d9fe3dc
NichlasMau <Nich5384@stud.kea.dk>
2022-09-12 11:35:20 +0200

    Oprettelse af klasser og objekter i superhelt klasse

---
+++ src/main/java/Superhelt.java
@@ -0,0 +1,17 @@
+public class Superhelt {
+
+    private String citizenName;
+    private String superheroName;
+    private String superPower;
+    private String creationYear;
+    private double strength;
+    private boolean isHuman = true;
+
+
+    public Superhelt(String citizenName, String superheroName, String superPower, String creationYear, double strength, boolean isHuman) {
+    }
+
+
+
+
+}
